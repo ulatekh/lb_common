077017278eb48c00cc8273018f13f09609e1eeac
Lee Baker <lee@leecbaker.com>
2019-07-09 22:13:41 +1200

    Add lb_filesystem

---
+++ lb_filesystem/lb_filesystem.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#if __APPLE__
+
+#include <boost/filesystem.hpp>
+namespace lb {
+    namespace filesystem {
+        using path = boost::filesystem::path;
+        using boost::filesystem::absolute;
+        using boost::filesystem::current_path;
+        using boost::filesystem::directory_iterator;
+        using boost::filesystem::exists;
+        using boost::filesystem::file_size;
+        using boost::filesystem::is_directory;
+        using boost::filesystem::is_regular_file;
+        using boost::filesystem::remove;
+        using boost::filesystem::rename;
+        using boost::filesystem::temp_directory_path;
+
+        using boost::filesystem::filesystem_error;
+    }
+}
+
+#else
+#include <filesystem>
+namespace lb {
+    namespace filesystem {
+        using path = std::filesystem::path;
+        using std::filesystem::absolute;
+        using std::filesystem::current_path;
+        using std::filesystem::directory_iterator;
+        using std::filesystem::exists;
+        using std::filesystem::file_size;
+        using std::filesystem::is_directory;
+        using std::filesystem::is_regular_file;
+        using std::filesystem::remove;
+        using std::filesystem::rename;
+        using std::filesystem::temp_directory_path;
+
+        using std::filesystem::filesystem_error;
+    }
+}
+#endif
+
